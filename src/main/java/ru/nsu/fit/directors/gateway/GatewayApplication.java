f9a6a5165ac8c1fc929cbb19ce7c4f257a0b3d15
Fagor <oleg-veber@mail.ru>
2023-10-23 10:55:18 +0700

    delete swagger from gateway

--- src/main/java/ru/nsu/fit/directors/gateway/GatewayApplication.java
+++ src/main/java/ru/nsu/fit/directors/gateway/GatewayApplication.java
@@ -1,29 +1,14 @@
 package ru.nsu.fit.directors.gateway;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.servers.Server;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
-
-import java.util.List;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class GatewayApplication {
-    @Value("${swagger.baseUrl}")
-    private String baseUrl;
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
-    }
-
-    @Bean
-    public OpenAPI customOpenApi() {
-        Server server = new Server();
-        server.setUrl(baseUrl);
-        return new OpenAPI().servers(List.of(server));
     }
 }
